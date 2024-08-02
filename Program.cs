d389292e79ac0d9f478444919ad6a40198c37aaf
DaviesNyantakyi <davies.nyantakyi@outlook.com>
2024-08-03 01:53:46 +0200

    Check leeftijd

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace BerekeningTarief
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+            int leeftijd;
+
+            leeftijd = int.Parse(Console.ReadLine());
+            if (leeftijd >= 18)
+            {
+                Console.WriteLine("Toegelaten");
+            }
+            else
+            {
+                Console.WriteLine("Niet toegelaten");
+            }
+
+            string check = leeftijd >= 18 ? "true" : "false";
+            Console.WriteLine(check);
+        }
+    }
+}
+
+
+
